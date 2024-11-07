1be888dc7bb16a6de1c65a71cbb46cbf51068e5b
Liliana <lilianacarlson.lrc@gmail.com>
2024-11-07 13:39:10 -0600

    omni import fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Omni.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Omni.java
@@ -1,9 +1,5 @@
 package org.firstinspires.ftc.teamcode;
 
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
